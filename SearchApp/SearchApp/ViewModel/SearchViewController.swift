8fed575534df9a9163ad69856d0ceac7c85b2d68
Yohhaaa <situatiooon@gmail.com>
2022-03-20 12:07:45 +0300

    refactoring code
    added access level

--- SearchApp/SearchApp/ViewModel/SearchViewController.swift
+++ SearchApp/SearchApp/ViewModel/SearchViewController.swift
@@ -5,17 +5,17 @@ class SearchViewController: UIViewController {
     //MARK: Properties
     
     @IBOutlet private weak var resultOfSearchTableView: UITableView!
-    
-    let searchController = UISearchController(searchResultsController: nil)
-    var filteredPictures = [ImageStruct]()
-    var isFiltered: Bool { return searchController.isActive && !searchBarIsEmpty }
-    var searchBarIsEmpty: Bool { guard let text = searchController.searchBar.text else {return false}
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var filteredPictures = [ImageStruct]()
+    private var isFiltered: Bool { return searchController.isActive && !searchBarIsEmpty }
+    private var searchBarIsEmpty: Bool { guard let text = searchController.searchBar.text else {return false}
         
         return text.isEmpty
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
         setup()
     }
     
@@ -25,7 +25,6 @@ class SearchViewController: UIViewController {
         
         let nib = UINib(nibName: "CustomCell", bundle: nil)
         view.backgroundColor = .lightGray
-        
         resultOfSearchTableView.register(nib, forCellReuseIdentifier: "CustomCell")
         resultOfSearchTableView.backgroundColor = .lightGray
         resultOfSearchTableView.delegate = self
@@ -42,8 +41,6 @@ class SearchViewController: UIViewController {
         
         navigationItem.searchController = searchController
     }
-    
-    
 }
     
 //MARK: tableView Methods
@@ -51,45 +48,45 @@ class SearchViewController: UIViewController {
 extension SearchViewController: UITableViewDelegate,UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as? CustomCell else {return UITableViewCell()}
         
         var pictureForCell: ImageStruct
-        cell.spinner.color = .white
-        cell.spinner.startAnimating()
-        
         
         if isFiltered{
+            cell.spinner.isHidden = false
+            cell.spinner.color = .white
+            cell.spinner.startAnimating()
+            
             pictureForCell = filteredPictures[indexPath.row]
             cell.customLabel.text = pictureForCell.nameOfImage
             cell.customImageView.backgroundColor = .gray
             cell.configure(with: pictureForCell.urlOfImage)
         }
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if isFiltered { return filteredPictures.count }
-        else {return 0}
+        
+        return 0
     }
-    
-    
 }
+
 //MARK: search settings
+
 extension SearchViewController: UISearchResultsUpdating {
     
     func updateSearchResults(for searchController: UISearchController) {
-        
         filterContentForSearch(searchController.searchBar.text!)
-        
     }
     
-    func filterContentForSearch(_ searchText: String) {
-        
+    private func filterContentForSearch(_ searchText: String) {
         filteredPictures = ImageStruct.pictures.filter({ (pic:ImageStruct) -> Bool in
             
-            return pic.nameOfImage.lowercased().contains(searchText.lowercased())
-        })
+            return pic.nameOfImage
+                .lowercased()
+                .contains(searchText.lowercased())})
         
         self.resultOfSearchTableView.reloadData()
     }
@@ -98,7 +95,5 @@ extension SearchViewController: UISearchResultsUpdating {
 //MARK: UISearchBarDelegate
 extension SearchViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        
     }
 }
-
--- SearchApp/SearchApp/Views/CustomCell.swift
+++ SearchApp/SearchApp/Views/CustomCell.swift
@@ -5,38 +5,31 @@ class CustomCell: UITableViewCell {
     @IBOutlet weak var customImageView: UIImageView!
     @IBOutlet weak var customLabel: UILabel!
     @IBOutlet weak var spinner: UIActivityIndicatorView!
-    @IBOutlet weak var customCellView: UIView!
+    @IBOutlet private weak var customCellView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         customCellView.backgroundColor = .lightGray
     }
-    
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
     override func prepareForReuse(){
         customImageView.image = nil
     }
-    
   
-    func configure(with urlString: String){
-        guard let url = URL(string: urlString) else {
-            return
-        }
+    func configure(with urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
         URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-            guard let data = data, error == nil else {
-                return
-            }
+            guard let data = data, error == nil else { return }
+            
             DispatchQueue.main.async {
                 let image = UIImage(data: data)
                 self?.customImageView?.image = image
                 self?.spinner.stopAnimating()
                 self?.spinner.isHidden = true
             }
+            
         }.resume()
     }
-    
 }
